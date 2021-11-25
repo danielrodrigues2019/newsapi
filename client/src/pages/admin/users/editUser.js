3d35bd0f533973b53578f3f4a7479d91b56aea57
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-11-25 11:32:31 -0300

    Create update user

--- client/src/pages/admin/users/editUser.js
+++ client/src/pages/admin/users/editUser.js
@@ -1,9 +1,151 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Box from '@material-ui/core/Box'
+import TextField from '@material-ui/core/TextField'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import FormControl from '@material-ui/core/FormControl'
+import Select from '@material-ui/core/Select'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
+import Button from '@material-ui/core/Button'
+import SaveIcon from '@material-ui/icons/Save'
+import api from '../../../services/api'
+import MenuAdmin from '../../../components/menu-admin'
+import { useParams } from 'react-router-dom'
+import Footer from '../../../components/footer-admin'
+
+const useStyles = makeStyles((theme) => ({
+  root: { display: 'flex' },
+  title: { flexGrow: 1 },
+  appBarSpacer: theme.mixins.toolbar,
+  content: { flexGrow: 1, height: '100vh', overflow: 'auto' },
+  container: { paddingTop: theme.spacing(2), paddingBottom: theme.spacing(4) },
+  paper: { padding: 35, display: 'flex', overflow: 'auto', flexDirection: 'column' },
+  formControl: { width: '100%' },
+  btnSuccess: { backgroundColor: 'green', color: '#fff', '&:hover': { backgroundColor: '#12b912' } },
+}))
 
 export default function EditUser() {
+  const classes = useStyles()
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [type, setType] = useState('')
+
+  const { idUser } = useParams()
+
+  useEffect(() => {
+    async function getUser() {
+      var response = await api.get('/api/users/details/' + idUser)
+
+      setName(response.data.user_name)
+      setEmail(response.data.user_email)
+      setPassword(response.data.user_password)
+      setType(response.data.user_type)
+    }
+    getUser()
+  }, [idUser])
+
+  async function handleSubmit() {
+    const data = {
+      user_name: name,
+      user_email: email,
+      user_password: password,
+      user_type: type,
+      _id: idUser,
+    }
+
+    if (name !== '' && email !== '' && password !== '' && type !== '') {
+      const response = await api.put('/api/users/:_id', data)
+
+      if (response.status === 200) {
+        window.location.href = '/admin/users'
+      } else {
+        alert('Erro ao Atualizar o usuário!')
+      }
+    } else {
+      alert('Por favor, preencha todos os dados!')
+    }
+  }
+
   return (
-    <div>
-      <h1>EditUser</h1>
+    <div className={classes.root}>
+      <MenuAdmin title={'USUÁRIOS'} />
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item sm={12}>
+              <Paper className={classes.paper}>
+                <h2>Atualização de Usuários</h2>
+                <Grid container spacing={3}>
+                  <Grid item xs={12} sm={12}>
+                    <TextField
+                      required
+                      id="user_name"
+                      name="user_name"
+                      label="Nome Completo"
+                      fullWidth
+                      autoComplete="name"
+                      variant="standard"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      id="user_email"
+                      name="user_email"
+                      label="E-mail"
+                      fullWidth
+                      autoComplete="email"
+                      variant="standard"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControl className={classes.formControl}>
+                      <InputLabel id="user_type">Tipo</InputLabel>
+                      <Select labelId="user_type" id="user_type" value={type} onChange={(e) => setType(e.target.value)}>
+                        <MenuItem value={1}>Administrador</MenuItem>
+                        <MenuItem value={2}>Gerente</MenuItem>
+                        <MenuItem value={3}>Redator</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <TextField
+                      type="password"
+                      required
+                      id="user_password"
+                      name="user_password"
+                      label="Senha"
+                      fullWidth
+                      autoComplete="password"
+                      variant="standard"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <Button variant="contained" onClick={handleSubmit} className={classes.btnSuccess}>
+                      <SaveIcon /> Salvar
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Box pt={4}>
+            <Footer />
+          </Box>
+        </Container>
+      </main>
     </div>
   )
 }
